625e2242ff409645cb937a5c9151eb9d43d5daee
Trịnh Duy Hưng <57101685+Hung-00@users.noreply.github.com>
2025-08-24 17:03:27 +0700

    ok

--- planogram-project-cdk/planogram_project_cdk/stacks/bucket_stack.py
+++ planogram-project-cdk/planogram_project_cdk/stacks/bucket_stack.py
@@ -1,11 +1,10 @@
 from aws_cdk import (
     Stack,
     aws_s3 as s3,
-    aws_s3_deployment as s3_deployment,
-    aws_s3_notifications as s3n,
     CfnOutput,
     custom_resources as cr,
     aws_iam as iam,
+    aws_s3_deployment as s3_deployment,
 )
 from constructs import Construct
 
@@ -16,100 +15,9 @@ class S3BucketCdkStack(Stack):
         self,
         scope: Construct,
         construct_id: str,
-        invoke_yolo_lambda_cdk_stack=None,
         **kwargs,
     ) -> None:
         super().__init__(scope, construct_id, **kwargs)
-
-        self.source_bucket = s3.Bucket(
-            self,
-            "planogram_source_bucket",
-            versioned=False,
-            # removal_policy=cdk.RemovalPolicy.DESTROY,
-            # auto_delete_objects=True,
-        )
-
-        s3_deployment.BucketDeployment(
-            self,
-            "CreateImageFolder",
-            destination_bucket=self.source_bucket,
-            sources=[s3_deployment.Source.data("images/", "")],
-        )
-
-        # if (
-        #     invoke_yolo_lambda_cdk_stack
-        #     and invoke_yolo_lambda_cdk_stack.invoke_yolo_function
-        # ):
-        #     # Grant S3 read permissions to Lambda
-        #     self.source_bucket.grant_read(
-        #         invoke_yolo_lambda_cdk_stack.invoke_yolo_function
-        #     )
-
-        #     # Add event notification for object creation
-        #     # self.source_bucket.add_event_notification(
-        #     #     s3.EventType.OBJECT_CREATED,
-        #     #     s3n.LambdaDestination(
-        #     #         invoke_yolo_lambda_cdk_stack.invoke_yolo_function
-        #     #     ),
-        #     #     s3.NotificationKeyFilter(prefix="test-images/", suffix=".jpg"),
-        #     # )
-
-        #     # invoke_yolo_lambda_stack.invoke_yolo.add_environment(
-        #     #     "SOURCE_BUCKET_NAME",
-        #     #     self.planogram_source_bucket.bucket_name
-        #     # )
-
-        #     notification_resource = cr.AwsCustomResource(
-        #         self,
-        #         "S3NotificationResource",
-        #         on_create=cr.AwsSdkCall(
-        #             service="S3",
-        #             action="putBucketNotificationConfiguration",
-        #             parameters={
-        #                 "Bucket": self.source_bucket.bucket_name,
-        #                 "NotificationConfiguration": {
-        #                     "LambdaFunctionConfigurations": [
-        #                         {
-        #                             "Id": "YOLOProcessingTrigger",
-        #                             "LambdaFunctionArn": invoke_yolo_lambda_cdk_stack.invoke_yolo_function.function_arn,
-        #                             "Events": ["s3:ObjectCreated:*"],
-        #                             "Filter": {
-        #                                 "Key": {
-        #                                     "FilterRules": [
-        #                                         {"Name": "prefix", "Value": "images/"},
-        #                                         {"Name": "suffix", "Value": ".jpg"},
-        #                                     ]
-        #                                 }
-        #                             },
-        #                         }
-        #                     ]
-        #                 },
-        #             },
-        #             physical_resource_id=cr.PhysicalResourceId.of(
-        #                 f"S3Notification-{self.source_bucket.bucket_name}"
-        #             ),
-        #         ),
-        #         on_delete=cr.AwsSdkCall(
-        #             service="S3",
-        #             action="putBucketNotificationConfiguration",
-        #             parameters={
-        #                 "Bucket": self.source_bucket.bucket_name,
-        #                 "NotificationConfiguration": {},
-        #             },
-        #         ),
-        #         policy=cr.AwsCustomResourcePolicy.from_statements(
-        #             [
-        #                 iam.PolicyStatement(
-        #                     actions=["s3:PutBucketNotificationConfiguration"],
-        #                     resources=[self.source_bucket.bucket_arn],
-        #                 )
-        #             ]
-        #         ),
-        #         # log_retention=logs.RetentionDays.ONE_DAY,
-        #     )
-
-        #     # Ensure the notification is created after the bucket
-        #     notification_resource.node.add_dependency(self.source_bucket)
 
         self.training_bucket = s3.Bucket(
             self,
@@ -158,20 +66,6 @@ class S3BucketCdkStack(Stack):
         # Tags.of(self.source_bucket).add("project", "planogram")
         # Tags.of(self.training_bucket).add("project", "planogram")
         # Tags.of(self.test_bucket).add("project", "planogram")
-
-        CfnOutput(
-            self,
-            "SourceBucketName",
-            value=self.source_bucket.bucket_name,
-            description="",
-        )
-
-        CfnOutput(
-            self,
-            "SourceBucketArn",
-            value=self.source_bucket.bucket_arn,
-            description="",
-        )
 
         CfnOutput(
             self,
--- planogram-project-cdk/planogram_project_cdk/stacks/invoke_yolo_lambda_stack.py
+++ planogram-project-cdk/planogram_project_cdk/stacks/invoke_yolo_lambda_stack.py
@@ -7,6 +7,9 @@ from aws_cdk import (
     RemovalPolicy,
     aws_lambda as lambda_,
     aws_iam as iam,
+    aws_s3_deployment as s3_deployment,
+    aws_s3_notifications as s3n,
+    aws_s3 as s3,
 )
 from constructs import Construct
 
@@ -16,6 +19,22 @@ class InvokeYOLOLambdaCdkStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        self.source_bucket = s3.Bucket(
+            self,
+            "planogram_source_bucket",
+            versioned=False,
+            removal_policy=RemovalPolicy.DESTROY,
+            # auto_delete_objects=True,
+            # block_public_access=s3.BlockPublicAccess.BLOCK_ALL
+        )
+
+        s3_deployment.BucketDeployment(
+            self,
+            "CreateImageFolder",
+            destination_bucket=self.source_bucket,
+            sources=[s3_deployment.Source.data("images/", "")],
+        )
+
         self.invoke_yolo_lambda_role = iam.Role(
             self,
             "invoke_yolo_lambda-role",
@@ -23,31 +42,44 @@ class InvokeYOLOLambdaCdkStack(Stack):
             managed_policies=[
                 iam.ManagedPolicy.from_aws_managed_policy_name(
                     "service-role/AWSLambdaBasicExecutionRole",
-                )
+                ),
+                iam.ManagedPolicy.from_aws_managed_policy_name("AmazonS3FullAccess"),
+                iam.ManagedPolicy.from_aws_managed_policy_name(
+                    "AmazonSageMakerFullAccess"
+                ),
+                iam.ManagedPolicy.from_aws_managed_policy_name("AWSLambda_FullAccess"),
+                iam.ManagedPolicy.from_aws_managed_policy_name(
+                    "AmazonBedrockFullAccess"
+                ),
+                iam.ManagedPolicy.from_aws_managed_policy_name("AmazonEC2FullAccess"),
+                iam.ManagedPolicy.from_aws_managed_policy_name("AmazonVPCFullAccess"),
+                iam.ManagedPolicy.from_aws_managed_policy_name(
+                    "SecretsManagerReadWrite"
+                ),
             ],
         )
 
-        self.invoke_yolo_lambda_role.add_managed_policy(
-            iam.ManagedPolicy.from_aws_managed_policy_name("AmazonS3FullAccess")
-        )
-        self.invoke_yolo_lambda_role.add_managed_policy(
-            iam.ManagedPolicy.from_aws_managed_policy_name("AmazonSageMakerFullAccess")
-        )
-        self.invoke_yolo_lambda_role.add_managed_policy(
-            iam.ManagedPolicy.from_aws_managed_policy_name("AWSLambda_FullAccess")
-        )
-        self.invoke_yolo_lambda_role.add_managed_policy(
-            iam.ManagedPolicy.from_aws_managed_policy_name("AmazonBedrockFullAccess")
-        )
-        self.invoke_yolo_lambda_role.add_managed_policy(
-            iam.ManagedPolicy.from_aws_managed_policy_name("AmazonEC2FullAccess")
-        )
-        self.invoke_yolo_lambda_role.add_managed_policy(
-            iam.ManagedPolicy.from_aws_managed_policy_name("AmazonVPCFullAccess")
-        )
-        self.invoke_yolo_lambda_role.add_managed_policy(
-            iam.ManagedPolicy.from_aws_managed_policy_name("SecretsManagerReadWrite")
-        )
+        # self.invoke_yolo_lambda_role.add_managed_policy(
+        #     iam.ManagedPolicy.from_aws_managed_policy_name("AmazonS3FullAccess")
+        # )
+        # self.invoke_yolo_lambda_role.add_managed_policy(
+        #     iam.ManagedPolicy.from_aws_managed_policy_name("AmazonSageMakerFullAccess")
+        # )
+        # self.invoke_yolo_lambda_role.add_managed_policy(
+        #     iam.ManagedPolicy.from_aws_managed_policy_name("AWSLambda_FullAccess")
+        # )
+        # self.invoke_yolo_lambda_role.add_managed_policy(
+        #     iam.ManagedPolicy.from_aws_managed_policy_name("AmazonBedrockFullAccess")
+        # )
+        # self.invoke_yolo_lambda_role.add_managed_policy(
+        #     iam.ManagedPolicy.from_aws_managed_policy_name("AmazonEC2FullAccess")
+        # )
+        # self.invoke_yolo_lambda_role.add_managed_policy(
+        #     iam.ManagedPolicy.from_aws_managed_policy_name("AmazonVPCFullAccess")
+        # )
+        # self.invoke_yolo_lambda_role.add_managed_policy(
+        #     iam.ManagedPolicy.from_aws_managed_policy_name("SecretsManagerReadWrite")
+        # )
 
         self.opencv_layer = lambda_.LayerVersion.from_layer_version_arn(
             self,
@@ -70,6 +102,23 @@ class InvokeYOLOLambdaCdkStack(Stack):
             #     "S3_MODEL_BUCKET": "a",
             # },
             description="Invoke YOLO",
+        )
+
+        # Grant Lambda permissions to read/write S3 bucket
+        self.source_bucket.grant_read_write(self.invoke_yolo_function)
+
+        # Add S3 trigger to Lambda
+        self.source_bucket.add_event_notification(
+            s3.EventType.OBJECT_CREATED,
+            s3n.LambdaDestination(self.invoke_yolo_function),
+            s3.NotificationKeyFilter(prefix="images/", suffix=".jpg"),
+        )
+
+        CfnOutput(
+            self,
+            "BucketName",
+            value=self.source_bucket.bucket_name,
+            description="S3 Bucket Name",
         )
 
         CfnOutput(
